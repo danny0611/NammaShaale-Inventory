33cc925cb3f97b9bdd455d277faded79ba441edc
danny0611 <dhanushdanny0611@gmail.com>
2026-05-13 13:29:35 +0530

    updated some changes

--- app/src/main/java/com/example/nammashaaleinventory/NammaShaaleApp.kt
+++
@@ -1,8 +0,0 @@
-package com.example.nammashaaleinventory
-
-import android.app.Application
-import com.example.nammashaaleinventory.data.InventoryRepository
-
-class NammaShaaleApp : Application() {
-    val repository by lazy { InventoryRepository() }
-}
